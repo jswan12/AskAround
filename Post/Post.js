79334615cffb0a61cf2fcbe79af598cfb2eb212b
Jacob Swanson <jswan12@lsu.edu>
2019-02-08 19:57:24 -0600

    add to the Post.html and Post.js

--- Post/Post.js
+++ Post/Post.js
@@ -1,13 +1,37 @@
 class Post{
     constructor(userID, title, description, cashAmount, ranking){
         this.userID = userID;
-        this.title = title;
-        this.description = "This is my description!";
-        this.description = description;
-        this.cashAmount = 0.00;
-        this.cashAmount = cashAmount;
+        if(title == "")
+            this.title = "This is my title!";
+        else
+            this.title = title;
+
+        if(description == "")
+            this.description = "This is my description!";
+        else
+            this.description = description;
+
+        if(cashAmount<=0)
+            this.cashAmount = 0.00;
+        else
+            this.cashAmount = cashAmount;
         this.ranking = ranking;
         console.log('Object Made with user ID: ', this.userID);
+    }
+    getUserId(){
+        return this.userID;
+    }
+    getTitle(){
+        return this.title;
+    }
+    getDescription(){
+        return this.description;
+    }
+    getCashAmount(){
+        return '$' + this.cashAmount;
+    }
+    getRanking(){
+        return 'Rank: ' + this.ranking;
     }
     toString(){
         return (this.userID + "\n" + "Title: " + this.title + "        " + "$" + this.cashAmount + "\n" + this.description);
