936a2f40982db5094d4d533c17bb93cf40c5d89b
Jacob Swanson <jswan12@lsu.edu>
2019-02-10 16:58:20 -0600

    create post's push to the database
    you can make a post and it will be pushed and stored here:
    https://console.firebase.google.com/project/askaround-01/database/askaround-01/data/

---
+++ js/functions.js
@@ -0,0 +1,36 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCpwgCgLOfU9l27TvFGVYkFQdeyJJnB-ck",
+    authDomain: "askaround-01.firebaseapp.com",
+    databaseURL: "https://askaround-01.firebaseio.com/",
+    projectId: "askaround-01",
+    storageBucket: "askaround-01.appspot.com",
+    messagingSenderId: "527860430663"
+    };
+    firebase.initializeApp(config);
+
+  // Reference to the post object in Firebase database
+  var posts = firebase.database().ref("Posts");
+  
+  // Save a new post to the database, using the input in the form
+  var submitPost = function () {
+
+  // Get input values from each of the form elements
+  var title = document.getElementById('title').value;
+  var description = document.getElementById('description').value;
+  var bounty = document.getElementById('price').value;
+  var category = document.getElementById('category').value;
+  
+    // Push a new post to the database using those values
+
+    posts.push({
+    "title": title,
+    "description": description,
+    "bounty": bounty,
+    "category": category
+    });
+  };
+
+  $(window).load(function () {
+    $("#postForm").submit(submitPost);
+  });
