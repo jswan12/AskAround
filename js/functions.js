773bf34da437e96ae4aa438073803fe7ba156722
Jacob Swanson <jswan12@lsu.edu>
2019-02-11 18:11:26 -0600

    fix what gio broke :+1:

--- js/functions.js
+++ js/functions.js
@@ -16,19 +16,20 @@
   var submitPost = function () {
 
   // Get input values from each of the form elements
-  var title = document.getElementById('title').value;
+  var question = document.getElementById('question').value;
   var description = document.getElementById('description').value;
   var bounty = document.getElementById('price').value;
   var category = document.getElementById('category').value;
   
     // Push a new post to the database using those values
 
-    posts.push({
-    "title": title,
+    var key = posts.push({
+    "question": question,
     "description": description,
     "bounty": bounty,
     "category": category
     });
+    console.log(key);
   };
 
   $(window).load(function () {
