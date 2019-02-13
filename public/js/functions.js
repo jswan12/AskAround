8f12ad783a58fca320bc2832896073d2b0a6c0ab
Jacob Swanson <jswan12@lsu.edu>
2019-02-12 19:54:46 -0600

    update public folder

--- public/js/functions.js
+++ public/js/functions.js
@@ -8,10 +8,7 @@
     messagingSenderId: "527860430663"
     };
     firebase.initializeApp(config);
-
-  // Reference to the post object in Firebase database
-  var posts = firebase.database().ref("Posts");
-  
+  var posts = firebase.database();
   // Save a new post to the database, using the input in the form
   var submitPost = function () {
 
@@ -20,18 +17,87 @@
   var description = document.getElementById('description').value;
   var bounty = document.getElementById('price').value;
   var category = document.getElementById('category').value;
+  // Reference to the post object in Firebase database
+  posts = posts.ref("Posts/" + category);
   
     // Push a new post to the database using those values
 
-    var key = posts.push({
+    posts.push({
     "question": question,
     "description": description,
     "bounty": bounty,
     "category": category
     });
-    console.log(key);
   };
 
-  $(window).load(function () {
+  function getPost(sub){
+    posts.ref("Posts/" + sub).on('value', gotData, noData);
+  }
+
+  function gotData(data){
+    try{
+      var keys = data.val();
+      var keysArray = Object.keys(keys);
+      keysArray.forEach(element => {
+        //var userName = keys[element].userName;
+        //console.log(keys[element]);
+        var question = keys[element].question;
+        var description = keys[element].description;
+        var bounty = keys[element].bounty;
+        var category = keys[element].category;
+        //console.log(question, "\n"+description, '\n'+bounty, "\n"+category);
+
+        var html = [
+          '<div class="post_topbar">',
+              '<div class="usy-dt">',
+              
+                  '<div class="usy-name">',
+                      '<h3>',
+                      'getUsername()',
+                      '</h3>',
+                      '<span><img src="images/clock.png" alt="">$',
+                      bounty,
+                      '</span>',
+                  '</div>',
+              '</div>',
+              '<div class="ed-opts">',
+                  '<a href="#" title="" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>',
+                  '<ul class="ed-options">',
+                      '<li><a href="#" title="">Edit Post</a></li>',
+                      '<li><a href="#" title="">Unsaved</a></li>',
+                      '<li><a href="#" title="">Unbid</a></li>',
+                      '<li><a href="#" title="">Close</a></li>',
+                      '<li><a href="#" title="">Hide</a></li>',
+                  '</ul>',
+              '</div>',
+          '</div>',
+          
+          '<div class="job_descp">',
+              '<h3>',
+              question,
+              '</h3>',
+              
+              '<p>',
+              description,
+              '</p>',
+              
+          '</div>'
+          ].join('');
+          var div = document.createElement('div');
+          div.setAttribute('class', 'post-bar');
+          div.innerHTML = html;
+          document.getElementById('posts-section').appendChild(div);
+      });
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+  function noData(error){
+    console.log("error!");
+    console.log(error);
+  }
+
+  $(window).load(function() {
     $("#postForm").submit(submitPost);
-  });
+  }, getPost(document.getElementById("pageTitle").innerText));
