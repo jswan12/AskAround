d2552d202cce5388a596b2631e0f8ab56e35b58b
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2019-02-17 15:31:12 -0600

    add firebase sdk to notifications.js

--- js/notifications.js
+++ js/notifications.js
@@ -1,4 +1,14 @@
-
+var config = {
+    apiKey: "AIzaSyCpwgCgLOfU9l27TvFGVYkFQdeyJJnB-ck",
+    authDomain: "askaround-01.firebaseapp.com",
+    databaseURL: "https://askaround-01.firebaseio.com/",
+    projectId: "askaround-01",
+    storageBucket: "askaround-01.appspot.com",
+    messagingSenderId: "527860430663"
+  };
+  
+  firebase.initializeApp(config);
+  
 //firebase notifications
 const messaging = firebase.messaging();
 
