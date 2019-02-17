4bb479902d75de4f93aebef4bc92a78b88c84f2d
Evan Hollier <evan.hollier98@gmail.com>
2019-02-16 21:13:37 -0600

    remove leftover comma

--- js/functions.js
+++ js/functions.js
@@ -131,4 +131,4 @@ var placeholderUID = "KuopYAm6v4Y2BpsJo2SR0JZ5lq83"; // Evan's
   $(window).load(function() {
     $("#postForm").submit(submitPost);
   }, getPost(document.getElementById("pageTitle").innerText), 
-     getRank(placeholderUID),  ); 
+     getRank(placeholderUID)  ); 
