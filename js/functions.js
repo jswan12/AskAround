a1e76e0d8bbbe1208b01a458801ca9fa39f46538
Jacob Swanson <jswan12@lsu.edu>
2019-04-08 02:52:44 -0500

    add if test to getPost()
    this if test will display that there are no active bounties if nothing exists in the category

--- js/functions.js
+++ js/functions.js
@@ -155,69 +155,79 @@ function pushUser(aUID, aName, aRank) {
 
 function getPost(sub) {
   dataBase.ref("Posts/" + sub).once('value', function (data) {
-    data.forEach(function (childSnapshot) {
-      var childData = childSnapshot.val();
-      var postID = childSnapshot.key;
-      var question = childData.question;
-      var description = childData.description;
-      var bounty = childData.bounty;
-      //category variable may not be used
-      var category = childData.category;
-      var visibility = childData.visibility;
-      var displayName = childData.displayName;
-      var myUid = childData.uid;
+    if(data.numChildren() === 0){
+      var html = 
+      ['<p style="text-align: center; margin-top: 115px;">','There are currently no active bounties for ',sub,'</p>'].join('');
+      var div = document.createElement('div');
+      div.setAttribute('id', 'noBounties');
+      div.innerHTML = html;
+      document.getElementById('posts-section').appendChild(div);
+    }
+    else{
+      data.forEach(function (childSnapshot) {
+        var childData = childSnapshot.val();
+        var postID = childSnapshot.key;
+        var question = childData.question;
+        var description = childData.description;
+        var bounty = childData.bounty;
+        //category variable may not be used
+        var category = childData.category;
+        var visibility = childData.visibility;
+        var displayName = childData.displayName;
+        var myUid = childData.uid;
 
-      if(visibility == 'visible'){
-        var html = [
-          '<div class="post_topbar">',
-          '<div class="usy-dt">',
+        if(visibility == 'visible'){
+          var html = [
+            '<div class="post_topbar">',
+            '<div class="usy-dt">',
 
-          '<div class="usy-name">',
-          '<h3>',
-          displayName,
-          '</h3>',
-          '<span>$',
-          bounty,
-          '</span>',
-          '</div>',
-          '</div>',
-          '<div class="ed-opts">',
-          '<a href="#" title="" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>',
-          '<input id="button',
-          postID,
-          '" type="button" value="Claim Bounty" onclick="openChatPage(\'',
-          postID,
-          '\');" style="float:right; background-color: green; color: white; height: 25px; width: 100px; border: none;"></input>',
-          '<ul class="ed-options">',
-          '<li><a href="#" title="">Edit Post</a></li>',
-          '<li><a href="#" title="">Unsaved</a></li>',
-          '<li><a href="#" title="">Unbid</a></li>',
-          '<li><a href="#" title="">Close</a></li>',
-          '<li><a href="#" title="">Hide</a></li>',
-          '</ul>',
-          '</div>',
-          '</div>',
+            '<div class="usy-name">',
+            '<h3>',
+            displayName,
+            '</h3>',
+            '<span>$',
+            bounty,
+            '</span>',
+            '</div>',
+            '</div>',
+            '<div class="ed-opts">',
+            '<a href="#" title="" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>',
+            '<input id="button',
+            postID,
+            '" type="button" value="Claim Bounty" onclick="openChatPage(\'',
+            postID,
+            '\');" style="float:right; background-color: green; color: white; height: 25px; width: 100px; border: none;"></input>',
+            '<ul class="ed-options">',
+            '<li><a href="#" title="">Edit Post</a></li>',
+            '<li><a href="#" title="">Unsaved</a></li>',
+            '<li><a href="#" title="">Unbid</a></li>',
+            '<li><a href="#" title="">Close</a></li>',
+            '<li><a href="#" title="">Hide</a></li>',
+            '</ul>',
+            '</div>',
+            '</div>',
 
-          '<div class="job_descp">',
-          '<h3>',
-          question,
-          '</h3>',
+            '<div class="job_descp">',
+            '<h3>',
+            question,
+            '</h3>',
 
-          '<p>',
-          description,
-          '</p>',
-          '</div>',
-          '<script></script>'
-        ].join('');
-        var div = document.createElement('div');
-        div.setAttribute('class', 'post-bar');
-        div.setAttribute('id', postID);
-        div.innerHTML = html;
-        document.getElementById('posts-section').appendChild(div);
-      }
-      /* MAKE SURE TO TAKE THIS LOG OUT LATER*/
-      else{console.log("hidding post " + postID);}
-    });
+            '<p>',
+            description,
+            '</p>',
+            '</div>',
+            '<script></script>'
+          ].join('');
+          var div = document.createElement('div');
+          div.setAttribute('class', 'post-bar');
+          div.setAttribute('id', postID);
+          div.innerHTML = html;
+          document.getElementById('posts-section').appendChild(div);
+        }
+        /* MAKE SURE TO TAKE THIS LOG OUT LATER*/
+        else{/*console.log("hidding post " + postID);*/}
+      });
+    }
   });
 }
 
