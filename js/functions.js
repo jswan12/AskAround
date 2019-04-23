811e0de0949ca1682d3e5ba0b736ab8437850175
Jacob Swanson <jswan12@lsu.edu>
2019-04-22 20:40:07 -0500

    update functions.js
    + add comments and tidy code

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,7 @@
-//firebase.auth().signInWithEmailAndPassword("priver3@lsu.edu", "password"); // placeholder account for offline testing
-
-const messaging = firebase.messaging();
-messaging.requestPermission().then(function() {
-  console.log('Have permission');
-  // return messaging.getToken();
-})
-// .then(function(token) {
-//   console.log(token + ' is the token');
-// })
-.catch(function(err){
-  console.log('Error, no permission');
-})
-
+//Initialize variables
 var dataBase = firebase.database();
-
 var uid = null;
 var displayName = null;
-
 var curChatA;
 
 // Save a new post to the database, using the input in the form
@@ -64,77 +49,88 @@ firebase.auth().onAuthStateChanged(function (user) {
   }
 });
 
+
+//when submitPost is called, it will run the function below
+//this function will send the Post attributes to our database
 var submitPost = function () {
+  //if the User does not have a active chat
   if(curChatA == 'null'){
-    ///channel update
+    //Update the User's channel data in the database
     var curChat = uid;
-    //var curPostId =
     var curPostType = document.getElementById('category').value;
-    console.log('curChat = '+curChat);
     firebase.database().ref('Users/' + uid ).update({curChat});
     firebase.database().ref('Users/' + uid ).update({curPostType});
-    ///end of channel update
+    ///end of User's channel update
     var myUid = uid;
     var mydisplayName = displayName;
     var question = document.getElementById('question').value;
     var description = document.getElementById('description').value;
     var bounty = document.getElementById('price').value;
-    //category variable may not be used
     var category = document.getElementById('category').value;
-    // Reference to the post object in Firebase database
     dataBase = dataBase.ref("Posts/" + category);
     // Push a new post to the database using those values
     dataBase.push({
       "uid": myUid,
-      "displayName": displayName,
+      "displayName": mydisplayName,
       "question": question,
       "description": description,
       "bounty": bounty,
       "category": category,
       "visibility": 'visible'
     });
-    console.log("I just posted");
-
+    //End of post push to database
+    //Open the loading page for the user.
     window.open("waiting.html");
   }
-  else{
+  //If the user tries to make a post while in another chat, they will be denied
+  else
     alert("You can only have one post at a time. Sorry.");
-  }
 };
 
+//This function returns the current user's rank from the database
 function getRank() {
   firebase.database().ref("Users/" + uid).once('value', function (data) {
       document.getElementById('userRank').innerText = data.val().Rank;
   }, function (error) { console.log(error); });
 }
 
+
+//This function injects html to the page to display that there are
+//currently no active posts for that category.
 function insertNoPost(sub){
+  //create the html to be injected
   var html = ['<p style="text-align: center; margin-top: 115px;">','There are currently no active bounties for ',sub,'</p>'].join('');
   var div = document.createElement('div');
   div.setAttribute('id', 'noBounties');
   div.innerHTML = html;
+  //add the div with the html elements inside to the posts section of the page
   document.getElementById('posts-section').appendChild(div);
 }
 
+//This function will display all of the active posts for a category
 function getPost(sub) {
   dataBase.ref("Posts/" + sub).once('value', function (data) {
+    //if there are no posts in the database under the category then say there are no posts
     if(data.numChildren() === 0)
       insertNoPost(sub)
     else{
+      //keep track of how many of the elements in this category are hidden
       var hiddenCount = 0
+      //for each post in this category grab all the data out out it
       data.forEach(function (childSnapshot) {
         var childData = childSnapshot.val();
         var postID = childSnapshot.key;
         var question = childData.question;
         var description = childData.description;
         var bounty = childData.bounty;
-        //category variable may not be used
         var category = childData.category;
         var visibility = childData.visibility;
         var displayName = childData.displayName;
         var myUid = childData.uid;
 
+        //If the post is listed as visible in the database then display it to the page
         if(visibility == 'visible'){
+          //create the html to be injected
           var html = [
             '<div class="post_topbar">',
               '<div class="usy-dt">',
@@ -164,10 +160,14 @@ function getPost(sub) {
           div.setAttribute('class', 'post-bar');
           div.setAttribute('id', postID);
           div.innerHTML = html;
+          //add the div with the html elements inside to the posts section of the page
           document.getElementById('posts-section').appendChild(div);
         }
+        //Increment count of hidden items
         else{hiddenCount++}
 
+        //If all posts within the category are hidden, then display that there are
+        //currently no posts for that category
         if(data.numChildren() === hiddenCount)
           insertNoPost(sub)
       });
@@ -175,81 +175,54 @@ function getPost(sub) {
   });
 }
 
+//This function will open the chat for the current User that claims the bounty
 function openChatPage(postKey) {
   var chan;
-  //if claiming posts is broken chance once to on
   firebase.database().ref('Users/' + uid).once('value', function(data){
-    //console.log(data.val().curChat)
+    //Make sure that the user claiming it is:
+    //1. not the user that posted it
+    //2. not in another chat
     if(data.val().curChat == "null"){
-      console.log('hello');
       firebase.database().ref('Posts/' + document.getElementById("pageTitle").innerText + '/' + postKey).once('value',
         function(data){
+          //get variables needed for payout
           var claimAmount = data.val().bounty;
           var visibility = data.val().visibility;
+          //update the post's visiblity state to hidden and update the channel of the claimer
           visibility = 'hidden';
-          ///channel update
           chan = data.val().uid;
           var curChat = chan;
           firebase.database().ref('Users/' + uid ).update({curChat});
           firebase.database().ref('Users/' + uid ).update({claimAmount});
-          ///end of channel update
-          console.log('chan = '+chan);
           firebase.database().ref('Posts/' + document.getElementById("pageTitle").innerText + '/' + postKey).update({visibility});
+          ///end of updates to database
+          //open the chat page for the claimer
           window.open("chat.html");
+          //reload the mainpage to insure that the visibility of the post is active
           document.location.reload(true);
         }, function (err) {
-          console.log("Could not set post " + postKey + " to hidden.");
+          console.log(err);
         }
       );
     }
-    /*else{
-      console.log(data.val().curChat);
-    }*/
+    else
+      alert("You cannot claim a bounty when in another chat or while waiting for your post to be claimed");
   });
 }
 
-// function deletePost(postKey) {
-//     if(confirm("Are you sure you want to delete post " + postKey + "?")){
-//       firebase.database().ref('Posts/' + document.getElementById("pageTitle").innerText + '/' + postKey).remove().then(function(){
-//         document.location.reload(true);
-//       }).catch(function(error){
-//         console.log('Delete Failed');
-//       });
-//     }
-//     console.log("Deletion Canceled");
-// }
-
-
-// //send email
-// function sendEmail(user) {
-//   // 5. Send welcome email to new users
-//   const mailOptions = {
-//           from: '"Dave" <dave@example.net>',
-//           to: '${user.email}',
-//           subject: 'Welcome!',
-//           html: `<YOUR-WELCOME-MESSAGE-HERE>`
-//   }
-//   // 6. Process the sending of this email via nodemailer
-//   return mailTransport.sendMail(mailOptions)
-//           .then(() => console.log('dbCompaniesOnUpdate:Welcome confirmation email'))
-//           .catch((error) => console.error('There was an error while sending the email:', error))
-// }
-
-
-//get notifications function
+//This function sets the notifications for a user
 function getNotification(sub) {
   dataBase.ref("Posts/" + sub).once('value', function (data) {
+    //For each post within the category, get the data
     data.forEach(function (childSnapshot) {
       var childData = childSnapshot.val();
-      var postID = childSnapshot.key;
       var question = childData.question;
-      var description = childData.description;
-      var bounty = childData.bounty;
-      var category = childData.category;//may not be used
       var visibility = childData.visibility;
       var displayName = childData.displayName;
 
+      //if the post has the attribute: visible then give the notification
       if(visibility == 'visible'){
+        //create the html to be injected
         var html = [
             '<div class="notification-info">',
               '<h3><a href="#" title="">', displayName, '</a></h3>',
@@ -259,16 +232,21 @@ function getNotification(sub) {
         var div = document.createElement('div');
         div.setAttribute('class', 'notification-details');
         div.innerHTML = html;
+        //add the div with the html elements inside to the posts section of the page
         document.querySelector('.nott-list').appendChild(div);
       }
     });
   });
 }
 
+//Get notifications for each category
 getNotification('Mathematics');
 getNotification('Computer Science');
 getNotification('Science');
 
+//When the window is loaded, add a eventlistener to the submit post button
+//to call the submit post function.
+//get the Posts and display them on the screnn when the page is loaded as well
 $(window).load(function () {
   $("#postForm").submit(submitPost);
 }, getPost(document.getElementById("pageTitle").innerText));
